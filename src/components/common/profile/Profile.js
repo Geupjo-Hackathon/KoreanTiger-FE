212a29736f73e1835d7f34755bd9fe85224dadf4
SeryeongK <99seryoung@naver.com>
2023-09-24 02:15:28 +0900

    feature :: 학생 메인 페이지 - 프로필 완료

--- src/components/common/profile/Profile.js
+++ src/components/common/profile/Profile.js
@@ -1,25 +1,23 @@
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import { AiFillStar } from "react-icons/ai";
-import color from "@/tailwind-theme/color";
 import { usePathname } from "next/navigation";
-import StudentProfileProgress from "@/components/student/profile/StudentProfileProgress";
+// Style
+import color from "@/tailwind-theme/color";
 
 const Profile = () => {
-  const username = "test";
-  const school = "test고등학교";
+  const [data, setData] = useState();
 
-  // const getProfile = async () => {
-  //   const response = await fetch(
-  //     `${process.env.BASE_URL}/studentLms/experience`
-  //   );
-  //   const jsonData = await response.json();
-  //   console.log(jsonData);
-  // };
+  const getProfile = async () => {
+    const response = await fetch(
+      `${process.env.BASE_URL}/studentLms/experience`
+    );
+    const jsonData = await response.json();
+    setData(jsonData.data);
+  };
 
-  // useEffect(() => {
-  //   getProfile();
-  // }, []);
+  useEffect(() => {
+    getProfile();
+  }, []);
+
   const [isStudent, setIsStudent] = useState(false);
   const pathname = usePathname();
 
@@ -29,28 +27,35 @@ const Profile = () => {
 
   return (
     <div
-      className="w-full h-32 flex flex-row justify-between p-4"
-      style={{ backgroundColor: "white" }}
+      className="w-full h-[7.5rem] flex flex-row justify-between p-4 rounded-md"
+      style={{ backgroundColor: color.hover_D9C3F1 }}
     >
-      <div className="w-1/3 h-full flex justify-center items-center">
-        사진
-        {/* <Image src={} alt="프로필 이미지"/> */}
+      <div className="w-2/5 h-full flex justify-center items-center">
+        <div
+          className="w-24 h-24 rounded-full flex justify-center items-center"
+          style={{ backgroundColor: "white" }}
+        >
+          <img src="/horang.png" />
+        </div>
       </div>
-      <div className="w-2/3 h-full flex flex-col">
+      <div className="w-3/5 h-full flex flex-col">
         {isStudent ? (
           <div>
-            <span className="text-lg">{username}</span>
-            <StudentProfileProgress />
+            <span className="text-3xl font-bold">{data && data.name}</span>
           </div>
         ) : (
           <div>
-            <span className="text-lg">{username} 선생님</span>
-            <div>{school}</div>
+            <span className="text-3xl font-bold">
+              {data && data.name} 선생님
+            </span>
           </div>
         )}
+        <div className="flex flex-row">
+          <p className="mr-4">레벨 LV{data && data.experience}</p>
+          <p>{data && data.title}</p>
+        </div>
         <div className="flex flex-row items-center">
-          <AiFillStar className="text-base mr-2" />
-          <p>0일 연속 접속</p>
+          <p>{data && data.connection}일 연속 접속</p>
         </div>
       </div>
     </div>
