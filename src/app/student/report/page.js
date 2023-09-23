5c6c31ee4a49c525abb6cef1cc5b86eae16add1e
SeryeongK <99seryoung@naver.com>
2023-09-23 16:12:17 +0900

    style :: 학생 페이지 레이아웃 변경

--- src/app/student/report/page.js
+++ src/app/student/report/page.js
@@ -1,19 +1,34 @@
 "use client";
 import Profile from "@/components/common/profile/Profile";
+import Hexagon from "@/components/common/report/Hexagon";
+import Alert from "@/components/common/alert/Alert";
+import Report from "@/components/common/report/Report";
+import Score from "@/components/common/report/Score";
+import GoodBad from "@/components/common/report/GoodBad";
+import Analysis from "@/components/common/report/Analysis";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-3 grid-rows-8 gap-4 max-h-screen min-w-screen p-[0.8rem]">
-      <div className="row-span-2 p-[0.8rem]">
+    <div
+      class="grid grid-cols-3 grid-rows-9 gap-0 max-h-screen min-w-screen p-[0.8rem]"
+      style={{ minHeight: "calc(100vh - 1.6rem)" }}
+    >
+      <div class="col-span-1 row-span-4 p-[0.8rem] flex flex-col justify-between">
         <Profile />
+        <Hexagon />
       </div>
-      <div className="row-span-3 col-start-1 row-start-3 p-[0.8rem]">2</div>
-      <div className="row-span-4 col-start-2 row-start-1 p-[0.8rem]">4</div>
-      <div className="col-start-3 row-start-1 p-[0.8rem]">5</div>
-      <div className="row-span-3 col-start-3 row-start-2 p-[0.8rem]">6</div>
-      <div className="row-span-3 col-start-1 row-start-6 p-[0.8rem]">7</div>
-      <div className="col-span-2 row-span-4 col-start-2 row-start-5 p-[0.8rem]">
-        8
+      <div class="col-span-1 row-span-3 p-[0.8rem]">
+        <Score />
+      </div>
+      <div class="col-span-1 row-span-3 p-[0.8rem] flex flex-col">
+        <Alert />
+        <Report />
+      </div>
+      <div class="col-span-2 row-span-6 p-[0.8rem]">
+        <Analysis />
+      </div>
+      <div class="col-span-1 row-span-5 p-[0.8rem]">
+        <GoodBad />
       </div>
     </div>
   );
