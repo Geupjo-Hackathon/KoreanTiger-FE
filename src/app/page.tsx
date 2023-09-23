25dfb970ddf68ffe13b9761d4f83488e7d8458b2
SeryeongK <99seryoung@naver.com>
2023-09-24 07:30:39 +0900

    style :: 인덱스 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 "use client";
+import color from "@/tailwind-theme/color";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Link href={"/student"} className="w-20 h-10 border">
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Link
+        href={"/student"}
+        className="w-50 h-30 text-3xl p-12 rounded-lg m-4 flex justify-center items-center"
+        style={{ backgroundColor: color.violet_9740F6, color: "white" }}
+      >
         학생
       </Link>
-      <Link href={"/teacher"} className="w-20 h-10 border">
+      <Link
+        href={"/teacher"}
+        className="w-50 h-30 text-3xl p-12 rounded-lg m-4 flex justify-center items-center"
+        style={{ backgroundColor: color.violet_9740F6, color: "white" }}
+      >
         선생님
       </Link>
     </div>
