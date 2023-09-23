18ab14ac339af62ca0138a594b617fb293881e27
SeryeongK <99seryoung@naver.com>
2023-09-23 21:52:18 +0900

    feature :: 성취도 연동

--- src/components/student/main/Progress.js
+++ src/components/student/main/Progress.js
@@ -1,51 +1,131 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// Component
 import { Button } from "@/stories/Button";
+// Style
+import color from "@/tailwind-theme/color";
 
 const Progress = () => {
+  const [data, setData] = useState({
+    lastWeek: {
+      weekOfMonth: 2,
+      totalLearningTime: 0,
+      completeLectureCount: 0,
+    },
+    thisWeek: {
+      weekOfMonth: 3,
+      totalLearningTime: 0,
+      completeLectureCount: 0,
+    },
+    meta: {
+      date: "2023-09-23",
+      achievementRate: "0",
+    },
+  });
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const getProgress = async () => {
+    const response = await fetch(
+      `${process.env.BASE_URL}/studentLms/achievement`
+    );
+    const jsonData = await response.json();
+    setData(jsonData.data);
+    console.log(jsonData.data);
+  };
+
+  useEffect(() => {
+    getProgress();
+  }, []);
+
   return (
-    <div className="w-full h-full p-6" style={{ backgroundColor: "white" }}>
+    <div
+      className="w-full h-full p-6 flex flex-col justify-between"
+      style={{ backgroundColor: "white" }}
+    >
       <div>
-        <p className="text-xl mb-4">주간 나의 성취도</p>
+        <p className="text-2xl mb-4 font-bold">주간 나의 성취도</p>
         <div className="w-full h-10 mb-2" style={{ backgroundColor: "gray" }}>
           <div
             className="h-10"
-            style={{ width: "45%", backgroundColor: "black" }}
+            style={{
+              width: `${
+                data.thisWeek.completeLectureCount &&
+                data.lastWeek.completeLectureCount &&
+                (data.thisWeek.completeLectureCount /
+                  data.lastWeek.completeLectureCount) *
+                  100
+              }%`,
+              backgroundColor: "black",
+            }}
           ></div>
         </div>
       </div>
       <div>
-        <div className="mb-4">
+        <div>
           <div className="flex flex-row items-center justify-between">
-            <p>00주: 00강</p>
+            <p>
+              {data.lastWeek.weekOfMonth}주차:
+              {data.lastWeek.completeLectureCount}강
+            </p>
             <div className="w-4/5 h-5">
               <div
                 className="h-5"
-                style={{ width: "45%", backgroundColor: "gray" }}
+                style={{
+                  width: `${data.lastWeek.completeLectureCount * 5}%`,
+                  backgroundColor: "gray",
+                }}
               ></div>
             </div>
           </div>
           <div className="flex flex-row items-center justify-between">
-            <p>00주: 00강</p>
+            <p>
+              {data.thisWeek.weekOfMonth}주차:
+              {data.thisWeek.completeLectureCount}강
+            </p>
             <div className="w-4/5 h-5">
               <div
                 className="h-5"
-                style={{ width: "45%", backgroundColor: "black" }}
+                style={{
+                  width: `${data.lastWeek.completeLectureCount * 5}%`,
+                  backgroundColor: "black",
+                }}
               ></div>
             </div>
           </div>
         </div>
       </div>
-      <div className="mb-4">
-        <p>0000년도 00주는 00주와 비교했을 때</p>
-        <p>000% 했습니다</p>
+      <div>
+        <p>
+          {data.meta.date.split("-")[0]}년도 {data.thisWeek.weekOfMonth}주차는{" "}
+          {data.lastWeek.weekOfMonth}주차와 비교했을 때
+        </p>
+        <p>
+          {data.thisWeek.completeLectureCount &&
+            data.lastWeek.completeLectureCount &&
+            (data.thisWeek.completeLectureCount /
+              data.lastWeek.completeLectureCount) *
+              100}
+          % 입니다
+        </p>
       </div>
       <div className="w-full flex flex-col">
-        <p className="mb-4">더 정확한 정보를 보려면 더보기를 눌러주세요</p>
+        <p className="mb-2 text-sm" style={{ color: color.violet_9740F6 }}>
+          더 정확한 정보를 보려면 더보기 버튼을 눌러주세요
+        </p>
         <Button
           primary={true}
-          backgroundColor="blue"
+          backgroundColor={isHovered && color.violet_9740F6}
           size="large"
           label="더보기"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
         />
       </div>
     </div>
