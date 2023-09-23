27cafbb95863144b31c38ed36fb2edc24cd79b97
SeryeongK <99seryoung@naver.com>
2023-09-24 02:36:26 +0900

    feature :: 학생 메인 페이지 - 출석체크 완료

--- src/components/student/main/Attendance.js
+++ src/components/student/main/Attendance.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 // Component
 import { Button } from "@/stories/Button";
+import color from "@/tailwind-theme/color";
 
 const Attendance = () => {
   const [isHovered, setIsHovered] = useState(false);
+  const [data, setData] = useState();
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -12,27 +14,35 @@ const Attendance = () => {
   const handleMouseLeave = () => {
     setIsHovered(false);
   };
-  // const getAttendance = async () => {
-  //   const response = await fetch(`${process.env.BASE_URL}/studentLms/check-in`);
-  //   const jsonData = await response.json();
-  //   console.log(jsonData);
-  // };
 
-  // useEffect(() => {
-  //   getAttendance();
-  // }, []);
+  const getAttendance = async () => {
+    const response = await fetch(`${process.env.BASE_URL}/studentLms/check-in`);
+    const jsonData = await response.json();
+    setData(jsonData.data);
+  };
+
+  useEffect(() => {
+    getAttendance();
+  }, []);
 
   return (
-    <div className="w-full h-2/3 flex p-4" style={{ backgroundColor: "white" }}>
+    <div
+      className="w-full h-2/3 flex p-4 border-2 rounded-md"
+      style={{ backgroundColor: "white", borderColor: color.hover_D9C3F1 }}
+    >
       <div
         className="w-24 h-full rounded-xl flex flex-col justify-center items-center"
-        style={{ backgroundColor: "gray" }}
+        style={{ backgroundColor: color.violet_9740F6 }}
       >
-        <p>TUE</p>
-        <p>00일</p>
+        <p className="text-lg font-bold" style={{ color: "white" }}>
+          {data && data.date.split("-")[1].slice(0, 3)}
+        </p>
+        <p className="text-lg font-bold" style={{ color: "white" }}>
+          {data && data.date.split("-")[0]}일
+        </p>
       </div>
       <div className="flex flex-col ml-4 w-full h-full">
-        <p className="text-lg">00일 연속 출석체크</p>
+        <p className="text-lg">{data && data.connection}일 연속 출석체크</p>
         <Button
           primary={true}
           backgroundColor={isHovered && color.violet_9740F6}
