7f7a0ece39e030646e418872505cbcfdff537e7a
SeryeongK <99seryoung@naver.com>
2023-09-23 21:51:32 +0900

    style :: 레이아웃, 색상, Button 컴포넌트 수정

--- src/tailwind-theme/color.js
+++ src/tailwind-theme/color.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pink_F6C8FB: `F6C8FB`,
+  pink_F6C8FB: `#F6C8FB`,
   hotpink_E784F2: `#E784F2`,
   blue_718DF2: `#718DF2`,
   jade_84FCED: `#84FCED`,
@@ -10,7 +10,7 @@ module.exports = {
   basic_EFEFEF: `#EFEFEF`,
   basic_CECECE: `#CECECE`,
   basic_6C6C6C: `#6C6C6C`,
-  basic_000000: `#000000`,
+  basic_2d363f: `#2d363f`,
 
   //hover color
   hover_FEF3FF: `#FEF3FF`,
