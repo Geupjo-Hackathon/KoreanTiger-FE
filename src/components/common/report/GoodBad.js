92e7021daa43f07f15b160d4a262f68b62d8c2e0
SeryeongK <99seryoung@naver.com>
2023-09-23 16:13:24 +0900

    style :: 분석 페이지 컴포넌트 추가

---
+++ src/components/common/report/GoodBad.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const GoodBad = () => {
+  return (
+    <div
+      className="w-full h-full flex flex-row p-4"
+      style={{
+        backgroundColor: "white",
+      }}
+    ></div>
+  );
+};
+
+export default GoodBad;
